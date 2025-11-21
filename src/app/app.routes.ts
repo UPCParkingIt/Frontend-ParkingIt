34ba7beca12285c02277ebf32f057a89542f5ec4
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2025-11-21 01:08:53 -0500

    feat: enable Angular routing and add Home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {SignUpComponent} from './iam/pages/sign-up/sign-up.component';
 import {SignInComponent} from './iam/pages/sign-in/sign-in.component';
+import {HomeComponent} from './public/pages/home.component/home.component';
 
 export const routes: Routes = [
   { path: "sign-up", component: SignUpComponent },
   { path: "sign-in", component: SignInComponent },
+  { path: "home", component: HomeComponent },
   { path: "", redirectTo: "sign-up", pathMatch: "full" },
   { path: "**", redirectTo: "sign-up" }
 ];
